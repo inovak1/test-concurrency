4258ff1f45c40be98613333f0dfcb69617113540
Ivan Novak <ivan.7.novak@gmail.com>
2019-04-24 20:54:27 +0200

    Added some example for working with threads

--- src/main/java/hr/test/concurrency/testconcurrency/service/TestService.java
+++ src/main/java/hr/test/concurrency/testconcurrency/service/TestService.java
@@ -1,9 +1,19 @@
 package hr.test.concurrency.testconcurrency.service;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
+	
+	static AtomicInteger counter = new AtomicInteger(0);
+	static Random rand = new Random();
+	
+	private Semaphore semaphore = new Semaphore(1);
+	private int sempahoreCounter = 0;
 	
 	public String testMethod() throws InterruptedException {
 
@@ -13,6 +23,44 @@ public class TestService {
 		//Simulate work being done
 		Thread.sleep(1500L);
 		
+		// randomizing a number to simulate chaotic behavior
+		int randomNumber = randInt(0, 5);
+		
+		if(randomNumber == 3) {
+			try {
+			
+			this.semaphore.acquire();
+			
+			System.out.println("Thread: " + Thread.currentThread().getName() + " got the resource. \n"
+					+ "As a proof, increasing the semaphore counter");
+			incrementSemaphoreCounter(randInt(0, 10));
+			System.out.println("Semaphore counter now at: " + this.sempahoreCounter);
+			
+			
+			Thread.sleep(100L);
+			
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			this.semaphore.release();
+		}
+		
+		System.out.println("Counter is at: " +  counter.getAndIncrement());
+		
+		
+		
 		return "Done";
+	}
+	
+	public static int randInt(int min, int max) {
+
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
+	
+	synchronized void incrementSemaphoreCounter(int number) {
+		this.sempahoreCounter = this.sempahoreCounter + number;
 	}
 }
